9360eb9a4b035d1db520e17b8dea0c70b5a4611c
d13gu1n <d.acevedom1993@gmail.com>
2025-11-28 13:13:34 -0300

    Added Console.Write() lines

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -2,6 +2,9 @@ class Activity1
 {
     public void Act1()
     {
-        Console.WriteLine("Hello World!");
+        // Console.WriteLine("Hello World!");
+        Console.Write("Congratulations!");
+        Console.Write(" ");
+        Console.Write("You wrote your first lines of code.");
     }
 }
