72b56d192bc657c6092d021a70d507ba7b35d46c
d13gu1n <d.acevedom1993@gmail.com>
2025-11-28 12:39:28 -0300

    First Hello World

---
+++ HelloWorld.cs
@@ -0,0 +1,7 @@
+class Activity1
+{
+    public void Act1()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// Activity 1
+
+Activity1 activity1 = new Activity1();
+activity1.Act1();
+
+// End of Activity 1 
