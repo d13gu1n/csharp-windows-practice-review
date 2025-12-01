df160b8d56ebad359ca4fbb05417893c0b6f3579
d13gu1n <d.acevedom1993@gmail.com>
2025-12-01 13:41:04 -0300

    Created Char activities and modified Program to run new Activity tests

---
+++ Chars.cs
@@ -0,0 +1,12 @@
+class Activity3
+{
+    public void Act3()
+    {
+        Console.WriteLine('b');
+        // Console.WriteLine('Hello World!');
+        Console.WriteLine(123);
+        Console.WriteLine(0.25F);
+        Console.WriteLine(2.625);
+        Console.WriteLine(12.39816m);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,14 @@ activity1.Act1(); */
 
 // Activity 2
 
-Activity2 activity2 = new Activity2();
-activity2.Act2();
+/* Activity2 activity2 = new Activity2();
+activity2.Act2(); */
 
 // End of Activity 2
+
+// Activity 3
+
+Activity3 activity3 = new Activity3();
+activity3.Act3();
+
+// End of Activity 3
