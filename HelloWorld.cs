4379854a71716ee4eff270cbfcdfe07c732fb807
d13gu1n <d.acevedom1993@gmail.com>
2025-12-01 11:33:08 -0300

    Created first Activity exrcise on C# Practice App

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -3,8 +3,16 @@ class Activity1
     public void Act1()
     {
         // Console.WriteLine("Hello World!");
-        Console.Write("Congratulations!");
-        Console.Write(" ");
+        Console.WriteLine("Congratulations!");
         Console.Write("You wrote your first lines of code.");
+    }
+}
+
+class Activity2
+{
+    public void Act2()
+    {
+        Console.WriteLine("This is the first line.");
+        Console.Write("This is the second line.");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 ﻿// Activity 1
 
-Activity1 activity1 = new Activity1();
-activity1.Act1();
+/* Activity1 activity1 = new Activity1();
+activity1.Act1(); */
 
 // End of Activity 1 
+
+
+// Activity 2
+
+Activity2 activity2 = new Activity2();
+activity2.Act2();
+
+// End of Activity 2
